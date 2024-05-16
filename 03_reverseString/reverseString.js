6cddde08b2fda6cd7b6d91c31a62ea81ebc890de
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-16 17:09:52 +0930

    reverseString complete.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let str1 = str;
+let str2 = "";
+i = str.length -1;
+while(i >= 0){
+    str2 += str1[i];
+    i-- ;
+    }
+return (str2)
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
