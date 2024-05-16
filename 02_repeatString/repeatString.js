e85b6252360a675023b92528c8748f55b6d6a1d4
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-16 14:44:45 +0930

    helloWorld completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    var str1 = str;
+    if(num == 0){
+        return('');
+    } else if(num < 0){
+        return "ERROR";
+    } else {
+    for(let n = 1; n < num; n++) {
+        str1 = str.concat(str1);
+    }}
+    return (str1);
 };
 
 // Do not edit below this line
