8f76d1f4e6136c52f70b75427d100f9103fcf439
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-16 18:01:05 +0930

    Completed first test. Reworked for 2nd test using 2 an index for arg[0] and an index for all args. Not yet working.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(args) {
+let index = 0;
+let i = 0;
+let totalArgs = arguments.length;
+let indexToRemove = index + 1;
+    for(args[index]; args[index] < totalArgs[i]; args[index++]){
+        args[0].splice(indexToRemove);
+    } return(args[0]);
 };
 
 // Do not edit below this line
