e59f070f4f73e3093c201ebeb3c9824e0aa6c2f6
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-23 12:16:10 +0930

    completed sumAll. Will tidy up once looked at solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
-const removeFromArray = function(args) {
-let index = 0;
-let i = 0;
-let totalArgs = arguments.length;
-let indexToRemove = index + 1;
-    for(args[index]; args[index] < totalArgs[i]; args[index++]){
-        args[0].splice(indexToRemove);
-    } return(args[0]);
-};
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    array.forEach((item) => {
+    if(!args.includes(item)) {
+        newArray.push(item);
+        };
+    });
+ return newArray;
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+let sum = 0;
+let temp = 0;
+if (int1 < 0 || int2 < 0) {
+    return "ERROR";
+} else if (typeof int1 != 'number'  || typeof int2 != 'number'){
+    return "ERROR";
+} else if (int1 > int2) {
+    temp = int1;
+    int1 = int2;
+    int2 = temp;
+} 
 
-};
+let start = int1;
+let numToCount = [];
+while(start != int2 + 1){
+    numToCount.push(start);
+    start++;
+}
+console.log(numToCount);
+numToCount.forEach( num => {
+    sum += num;
+})
+return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
