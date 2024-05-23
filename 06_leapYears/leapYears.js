a67c631ed0682e043fe98bda0dd3c4fecd8fdf20
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-23 13:11:20 +0930

    Completed leapYears. EZ ;)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+ leap           = year % 4;
+ nonLeapCentury = year % 100;
+ leapCentury    = year % 400;
+    if (leap == 0 && nonLeapCentury != 0 || leapCentury == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
