f07b6a6e9e129219050d3029ae97b0e33308bdda
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-23 13:58:54 +0930

    Completed using .toFixed and parseFloat to remove quotations marks from result.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degFar) {
+  newDegCel = (degFar - 32) * (5 / 9);
+  if (newDegCel % 1 == 0) {
+  return (newDegCel);
+  } else {
+    newDegCel = newDegCel.toFixed(1)
+  } return parseFloat(newDegCel);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degCel) {
+  newDegFar = ((degCel * 9 / 5) + 32);
+  if (newDegFar % 1 == 0){
+    return (newDegFar);
+  } else {
+    newDegFar = newDegFar.toFixed(1)
+  return parseFloat(newDegFar);
+}
 };
 
 // Do not edit below this line
