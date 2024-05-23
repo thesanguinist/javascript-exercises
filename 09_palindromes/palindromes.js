e5501e217a80766d866786e2e4a7b362807170f2
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-23 17:09:27 +0930

    Kinda script kiddied that one.. Didn't look at solution though.. I bet this is gonna bite me XD

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    str = str.toLowerCase();
+    return str == str.split('').reverse().join('');
 };
 
 // Do not edit below this line
