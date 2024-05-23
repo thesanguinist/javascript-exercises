46e53235e585164b056f1c01e6ffc102df7be608
thesanguinist <96358382+thesanguinist@users.noreply.github.com>
2024-05-23 14:37:00 +0930

    Trying reduce and for loop on sum, cannot yet meet all criteria. For reduce cannot compute empty string, for for loop, cannot computearray with 2 digits without excess code.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(num1, num2) {
+	result = num1 + num2;
+  return result;
+};
+
+const subtract = function(num1, num2) {
+	result = num1 - num2;
+  return result;
+};
+
+const sum = function(array) {
+  let accumulator = 0;
+  let index = 0;
+  let result = array.reduce((accumulator, index) => accumulator + index);
+//  for(let index = 0; index < array.length; index++){
+//    if (array.length == 1){
+//      result = array[0];
+//    } else
+//  result += array[i];
+  return result;
+};
+
+const multiply = function(array) {
+
+};
+
+const power = function(num1, num2) {
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
 	
 };
 
